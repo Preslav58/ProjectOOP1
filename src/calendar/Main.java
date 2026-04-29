6e59f9e58e8d6fb1d7461e661831eb003e7efc8d
Preslav58 <pindikov.preslav@gmail.com>
2026-04-29 16:13:10 +0300

    Оправих модела като добавих class day и timeInterval , останалите промени са просто където се прави нов Event.

--- src/calendar/Main.java
+++ src/calendar/Main.java
@@ -5,7 +5,6 @@ import calendar.console.Engine;
 import calendar.console.comand.CommandFactory;
 import calendar.fileManeger.TextStorage;
 
-
 public class Main {
     public static void main(String[] args) {
         TextStorage textStorage = new TextStorage();
